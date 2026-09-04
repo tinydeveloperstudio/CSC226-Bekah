df2461f15e07614b6006a0d0146327273c735a0a
Beck (Bekah) <thetinydeveloperstudio@gmail.com>
2026-09-04 18:07:44 +0000

    I added the private int count, the constructor methods, the count accessor method, the update and overloaded update methods, the reset counter method, and the private method that records the count of the counter and stores it into an array of a preset size. All of these were added to the Counter class.

--- InClassNotes/src/main/java/week1/Counter.java
+++ InClassNotes/src/main/java/week1/Counter.java
@@ -1,21 +1,40 @@
 package week1;
 
+import java.util.List;
+
 public class Counter {
 
     // 2. Add a private field
-    // TODO: Declare private int count;
-
+    private int count = 0;
     // 3. Write the default constructor
-
+    public Counter{
+    }
     // 4. Add an alternate constructor
-
+    public Counter(string CounterName){
+    }
     // 5. Write an accessor method
-
+    public int GetCount(){
+        return count;
+    }
     // 6. Add an update method
-    
+    public void Update(){
+        RecordSize();
+        count++;
+    }
     // 7. Overload increment
-   
+    public void Update(int CountIncrement){
+        RecordSize();
+        count = count + CountIncrement;
+    }
     // 8. Add a reset method
-
+    public void ResetCount(){
+        count = 0;
+    }
     //9. Add a fixed-size history array that records the last 5 states of the counter. All logic must happen inside of the object and be hidden from the user.
+    private void RecordSize(){
+        int[] historyList = new Counter[5];
+        for (int i = 0; i <= 4; i++){
+            historyList[i] = GetCount();
+        }
+    }
 }
